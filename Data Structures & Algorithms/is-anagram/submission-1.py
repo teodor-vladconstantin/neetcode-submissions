27475655da69af17731cc4e5e2ca42d85a3fa886
Teodor-Vlad Constantin <teodor-vlad.constantin@bphasdeu.ro>
2026-09-06 14:45:44 +0300

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False;
+        
+        hashS, hashT = {}, {}
+
+        for i in range(len(s)):
+            hashS[s[i]] = 1 + hashS.get(s[i], 0)
+            hashT[t[i]] = 1 + hashT.get(t[i], 0)
+        return hashS == hashT
